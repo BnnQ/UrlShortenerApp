54d6243364cca1b3330efb124faa7b1fc8b5c06f
BnnQ <1v25fa94@gmail.com>
2023-05-24 16:38:12 +0300

    Added Azure clients

--- Utils/Startup.cs
+++ Utils/Startup.cs
@@ -3,6 +3,7 @@ using System.IO;
 using Azure.Data.Tables;
 using Azure.Storage.Queues;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using UrlShortenerApp.Services;
@@ -17,38 +18,48 @@ public class Startup : FunctionsStartup
 {
     public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
     {
-        builder.ConfigurationBuilder.AddJsonFile(Path.Combine(builder.GetContext().ApplicationRootPath, "appsettings.json"));
+        builder.ConfigurationBuilder.AddJsonFile(Path.Combine(builder.GetContext()
+            .ApplicationRootPath, "appsettings.json"));
     }
 
     public override void Configure(IFunctionsHostBuilder builder)
     {
         var configuration = builder.GetContext()
             .Configuration;
-        
+
+        const string QueueConnectionStringName = "QueueAccount";
+        var queueConnectionString = configuration.GetConnectionString(QueueConnectionStringName) ??
+                                    throw new InvalidOperationException(
+                                        $"'{QueueConnectionStringName}' connection string is not provided.");
+
+        const string TableConnectionStringName = "TableAccount";
+        var tableConnectionString = configuration.GetConnectionString(TableConnectionStringName) ??
+                                    throw new InvalidOperationException(
+                                        $"'{TableConnectionStringName}' connection string is not provided.");
+
+        builder.Services.AddAzureClients(clients =>
+        {
+            clients.AddQueueServiceClient(queueConnectionString);
+            clients.AddQueueServiceClient(tableConnectionString);
+        });
+
         builder.Services.AddSingleton<TableClient>(_ =>
         {
-            const string ConnectionStringName = "TableAccount";
-            var connectionString = configuration.GetConnectionString(ConnectionStringName) ??
-                                   throw new InvalidOperationException($"'{ConnectionStringName}' connection string is not provided.");
-
             const string TableNameConfigurationPath = "Azure:Table:Name";
             var tableName = configuration[TableNameConfigurationPath];
 
-            var tableClient = new TableClient(connectionString, tableName);
+            var tableClient = new TableClient(tableConnectionString, tableName);
             tableClient.CreateIfNotExists();
             return tableClient;
         });
 
         builder.Services.AddSingleton<QueueClient>(_ =>
         {
-            const string ConnectionStringName = "QueueAccount";
-            var connectionString = configuration.GetConnectionString(ConnectionStringName) ??
-                                   throw new InvalidOperationException($"'{ConnectionStringName}' connection string is not provided.");
-
             const string QueueNameConfigurationPath = "Azure:Queue:Name";
             var queueName = configuration[QueueNameConfigurationPath];
 
-            var queueClient = new QueueClient(connectionString, queueName);
+            var queueClient = new QueueClient(queueConnectionString, queueName,
+                new QueueClientOptions { MessageEncoding = QueueMessageEncoding.Base64 });
             queueClient.CreateIfNotExists();
             return queueClient;
         });
@@ -59,5 +70,4 @@ public class Startup : FunctionsStartup
         builder.Services.AddTransient<ITextEntropier, UpperLowerCaseTextEntropier>();
         builder.Services.AddTransient<IUrlShortener, RandomLetterUrlShortener>();
     }
-    
 }

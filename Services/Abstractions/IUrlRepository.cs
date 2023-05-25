df82a4cdfaae58cdcbabdecbaed90ef1cacdc5f3
BnnQ <1v25fa94@gmail.com>
2023-05-25 15:57:07 +0300

    Added IUrlRepository

---
+++ Services/Abstractions/IUrlRepository.cs
@@ -0,0 +1,15 @@
+#nullable enable
+using System.Threading.Tasks;
+using UrlShortenerApp.Models.Entities;
+
+namespace UrlShortenerApp.Services.Abstractions;
+
+public interface IUrlRepository
+{
+    public Task<Url?> GetUrlByFullUrlIfExistsAsync(string fullUrl);
+    public Task<Url?> GetUrlByShortcutCodeIfExistsAsync(string shortcutCode);
+    public Task AddUrlAsync(Url url);
+    public Task UpdateUrlAsync(Url url);
+    public Task<int> GetCurrentIdentifierAsync();
+    public Task UpdateIdentityAsync(int newIdentifier);
+}
